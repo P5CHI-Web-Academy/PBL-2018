f90c5f7789f823c42a738e0a4307345c80297b17
iondodon1998 <iondodon2@gmail.com>
2018-11-07 12:39:11 +0200

    slide deletion implemented

--- src/AdministrationBundle/Controller/SlidesController.php
+++ src/AdministrationBundle/Controller/SlidesController.php
@@ -8,7 +8,6 @@ use AdministrationBundle\Entity\Slide;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class SlidesController extends Controller
 {
     /**
@@ -26,4 +25,35 @@ class SlidesController extends Controller
             array('slides' => $slides)
         );
     }
+
+    /**
+     * @Route("/admin/slides/deletion/{id}", name="slide_delete")
+     */
+    public function deleteSlide($id){
+        $entityManaget = $this->getDoctrine()->getManager();
+
+        $slide = $this->getDoctrine()
+            ->getRepository(Slide::class)
+            ->find($id);
+
+        $entityManaget->remove($slide);
+        $entityManaget->flush();
+
+        return $this->redirectToRoute("slides");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
