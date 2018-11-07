5cd7a1e6b70d1459ab2ca875e9bc5aec4b689819
eejova <eejova@pentalog.com>
2018-11-07 17:25:34 +0200

    7 november 2018

--- src/AdministrationBundle/Controller/SecurityController.php
+++ src/AdministrationBundle/Controller/SecurityController.php
@@ -2,22 +2,14 @@
 
 namespace AdministrationBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 
 class SecurityController extends Controller
 {
     /**
-     * @Template("AdministrationBundle:Security:login_form.html.twig")
      * @Route("/login", name="login")
      */
     public function loginAction(AuthenticationUtils $authenticationUtils)
@@ -28,39 +20,13 @@ class SecurityController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ];
-
-        /** @var $session Session */
-        $session = $request->getSession();
-        $authErrorKey = Security::AUTHENTICATION_ERROR;
-        $lastUsernameKey = Security::LAST_USERNAME;
-
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has($authErrorKey)) {
-            $error = $request->attributes->get($authErrorKey);
-        } elseif (null !== $session && $session->has($authErrorKey)) {
-            $error = $session->get($authErrorKey);
-            $session->remove($authErrorKey);
-        } else {
-            $error = null;
-        }
-        if (!$error instanceof AuthenticationException) {
-            $error = null; // The value does not come from the security component.
-        }
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
-        $csrfToken = $this->tokenManager
-            ? $this->tokenManager->getToken('authenticate')->getValue()
-            : null;
-        return $this->renderLogin(array(
-            'last_username' => $lastUsername,
-            'error' => $error,
-            'csrf_token' => $csrfToken,
-        ));
-
+        return $this->render(
+            '@Administration/Security/login_form.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]
+        );
     }
 
     public function checkAction()
@@ -72,9 +38,5 @@ class SecurityController extends Controller
     {
         throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
     }
-
-    protected function renderLogin(array $data)
-    {
-        return $this->render('@FOSUser/Security/login.html.twig', $data);
-    }
 }
+
