b8db42d1cff83300693a4d1b6f0ad83eebb61bc2
iondodon1998 <iondodon2@gmail.com>
2019-01-15 19:46:59 +0200

    first commit

--- src/AdministrationBundle/Controller/SlidesController.php
+++ src/AdministrationBundle/Controller/SlidesController.php
@@ -24,11 +24,10 @@ class SlidesController extends Controller
         $slides = $this->getDoctrine()->getRepository(Slide::class)->findAll();
 
         $logged_in_user = $this->getUser();
-        $logged_in_user_id = $logged_in_user->getId();
 
         return $this->render('@Administration/Slides/listOfSlides.html.twig',[
             'slides' => $slides,
-            'logged_in_user_id' => $logged_in_user_id
+            'logged_in_user' => $logged_in_user
         ]);
     }
     
